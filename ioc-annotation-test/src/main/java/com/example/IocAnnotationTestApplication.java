2db08ea1058e9e61ad933944eeceb17e47142ec9
SiouWang <w8774227@gmail.com>
2018-08-28 20:55:27 +0800

    测试AnnotationConfigApplicationContext
    利用AnnotationConfigApplicationContext的多个构造方法进行容器启动测试

--- ioc-annotation-test/src/main/java/com/example/IocAnnotationTestApplication.java
+++ ioc-annotation-test/src/main/java/com/example/IocAnnotationTestApplication.java
@@ -1,11 +1,36 @@
 package com.example;
 
+import com.example.bean.User;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class IocAnnotationTestApplication {
 
     public static void main(String args[]) {
+
+        System.out.println("=======================================================================");
+
+        usingAnnotatedClasses();
+
+
+    }
+
+    /**
+     * 使用带注解的Bean来作为容器参数启动
+     */
+    public static void usingAnnotatedClasses() {
+        ApplicationContext context = new AnnotationConfigApplicationContext(User.class);
+        // 获取IOC容器内的所有BeanDefinition
+        String[] beanNames = context.getBeanDefinitionNames();
+        for(String str : beanNames) {
+            System.out.println("当前容器中的bean有：" + str);
+        }
+    }
+
+    /**
+     * 使用包路径作为容器参数来启动
+     */
+    public static void usingBasePackages() {
         // 使用基于注解的IOC容器
         ApplicationContext context = new AnnotationConfigApplicationContext("com.example.*");
         // 获取IOC容器内的所有BeanDefinition
@@ -13,7 +38,6 @@ public class IocAnnotationTestApplication {
         for(String str : beanNames) {
             System.out.println("当前容器中的bean有：" + str);
         }
-
     }
 
 }
